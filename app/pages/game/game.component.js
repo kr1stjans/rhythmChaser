a17aa4118a37ede92abbfcbc9b8844dba72e048c
kr1stjans <kristjansesek@gmail.com>
2016-11-16 10:45:19 +0100

    Sync gestures with actual beats

--- app/pages/game/game.component.js
+++ app/pages/game/game.component.js
@@ -1,36 +1,110 @@
 "use strict";
 var core_1 = require("@angular/core");
-var zscore_1 = require("../../shared/detection_algorithms/zscore");
-var kalmanjs_1 = require("../../shared/detection_algorithms/kalmanjs");
+var page_1 = require("ui/page");
 var nativescript_accelerometer_1 = require("nativescript-accelerometer");
 var nativescript_audio_1 = require('nativescript-audio');
+var zscore_1 = require("../../shared/detection_algorithms/zscore");
+var kalmanjs_1 = require("../../shared/detection_algorithms/kalmanjs");
+var label_1 = require("ui/label");
+var absolute_layout_1 = require("ui/layouts/absolute-layout");
+var file_system_1 = require("file-system");
 var GameComponent = (function () {
-    function GameComponent(zone) {
+    function GameComponent(zone, page) {
         this.zone = zone;
+        this.page = page;
         this.lastValue = 0;
         this.buffer = [];
         this.zscore = new zscore_1.zScore(5, 3.5, 0.1);
         this.kalman = new kalmanjs_1.KalmanFilter({ R: 5, Q: 0.01 });
+        // animation variables
+        this.animationPositions = [20, 60, 100, 140, 180, 220];
+        this.animationDirection = AnimationDirection.INCREASING;
+        this.animationPosition = this.animationPositions.length / 2;
+        // score variables
+        this.score = 0;
+        this.scoreMultiplier = 1;
+        this.gestureMade = false;
     }
     GameComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.score = 0;
-        this.scoreModel = 0;
+        this.scoresLayout = this.page.getViewById("scoresLayout");
         this.player = new nativescript_audio_1.TNSPlayer();
-        this.playAudio('~/audio/faded.mp3', 'localFile');
-        nativescript_accelerometer_1.startAccelerometerUpdates(function (data) {
-            _this.zone.run(function () {
-                var result = _this.naiveFilter(data.z);
-                var output = _this.detectSignalHigh(result);
-                if (output == 1) {
-                    _this.scoreModel += 50;
-                }
-                //console.log(data.z);
+        // read beats file
+        file_system_1.knownFolders.currentApp().getFile("/audio/faded.csv").readText().then(function (text) {
+            _this.beatArray = text.split(",").map(function (value) {
+                return +value;
+            });
+        }).then(function () {
+            // start playing song
+            _this.playAudio("~/audio/faded.mp3", "localFile");
+            _this.startTime = new Date().getTime();
+            // start accelerometer updates
+            nativescript_accelerometer_1.startAccelerometerUpdates(function (data) {
+                _this.zone.run(function () {
+                    var result = _this.naiveFilter(data.z);
+                    var signalHigh = _this.detectSignalHigh(result);
+                    if (signalHigh) {
+                        _this.gestureMade = true;
+                    }
+                    //console.log(data.z);
+                });
             });
         });
-        // score on the view must be manually updated every 100ms, or it overuses the cpu
+        // view must be manually updated every 100ms or it overuses the cpu and no update is processed
         setInterval(function () {
-            _this.score = _this.scoreModel;
+            // if gesture was made check if actual beat exists
+            if (_this.gestureMade) {
+                var actualBeatExists = false;
+                var timeSinceStartInSeconds = (new Date().getTime() - _this.startTime) / 1000;
+                for (var _i = 0, _a = _this.beatArray; _i < _a.length; _i++) {
+                    var beat = _a[_i];
+                    // beat at current time detected
+                    var diff = Math.abs(beat - timeSinceStartInSeconds);
+                    if (diff < 0.11) {
+                        actualBeatExists = true;
+                        break;
+                    }
+                }
+                var label = new label_1.Label();
+                label.cssClass = "big-text score-label";
+                label.text = "+" + (actualBeatExists ? _this.scoreMultiplier : 0);
+                absolute_layout_1.AbsoluteLayout.setLeft(label, _this.animationPositions[_this.animationPosition]);
+                absolute_layout_1.AbsoluteLayout.setTop(label, 425);
+                _this.scoresLayout.addChild(label);
+                var animation = label.createAnimation({
+                    translate: { x: 0, y: -425 },
+                    opacity: 0.5,
+                    duration: 2000,
+                    scale: { x: 0.5, y: 0.5 },
+                });
+                animation.play().then(function () {
+                    _this.scoresLayout.removeChild(label);
+                });
+                if (actualBeatExists) {
+                    _this.score += _this.scoreMultiplier;
+                    _this.scoreMultiplier++;
+                }
+                else {
+                    _this.scoreMultiplier = 1;
+                }
+                // adjust position
+                if (_this.animationDirection == AnimationDirection.INCREASING && _this.animationPosition < _this.animationPositions.length - 1) {
+                    _this.animationPosition++;
+                }
+                else if (_this.animationDirection == AnimationDirection.INCREASING && _this.animationPosition == _this.animationPositions.length - 1) {
+                    _this.animationPosition--;
+                    _this.animationDirection = AnimationDirection.DECREASING;
+                }
+                else if (_this.animationDirection == AnimationDirection.DECREASING && _this.animationPosition > 0) {
+                    _this.animationPosition--;
+                }
+                else if (_this.animationDirection == AnimationDirection.DECREASING && _this.animationPosition == 0) {
+                    _this.animationPosition++;
+                    _this.animationDirection = AnimationDirection.INCREASING;
+                }
+                // reset beat detection states
+                _this.gestureMade = false;
+            }
         }, 100);
     };
     GameComponent.prototype.playAudio = function (filepath, fileType) {
@@ -87,7 +161,7 @@ var GameComponent = (function () {
         return result[result.length - 1];
     };
     GameComponent.prototype.naiveFilter = function (input) {
-        if (input > 0.25) {
+        if (input > 0.185) {
             return 1;
         }
         else {
@@ -97,10 +171,10 @@ var GameComponent = (function () {
     GameComponent.prototype.detectSignalHigh = function (input) {
         var result;
         if (this.lastValue == 0 && input == 1) {
-            result = 1;
+            result = true;
         }
         else {
-            result = 0;
+            result = false;
         }
         this.lastValue = input;
         return result;
@@ -110,9 +184,14 @@ var GameComponent = (function () {
             selector: "game",
             templateUrl: "pages/game/game.html"
         }), 
-        __metadata('design:paramtypes', [core_1.NgZone])
+        __metadata('design:paramtypes', [core_1.NgZone, page_1.Page])
     ], GameComponent);
     return GameComponent;
 }());
 exports.GameComponent = GameComponent;
+var AnimationDirection;
+(function (AnimationDirection) {
+    AnimationDirection[AnimationDirection["INCREASING"] = 0] = "INCREASING";
+    AnimationDirection[AnimationDirection["DECREASING"] = 1] = "DECREASING";
+})(AnimationDirection || (AnimationDirection = {}));
 //# sourceMappingURL=game.component.js.map
--- app/pages/game/game.component.ts
+++ app/pages/game/game.component.ts
@@ -1,54 +1,141 @@
 import {Component, NgZone, OnInit} from "@angular/core";
-import {zScore} from "../../shared/detection_algorithms/zscore";
-import {KalmanFilter} from "../../shared/detection_algorithms/kalmanjs";
+import {Page} from "ui/page";
+
 import {startAccelerometerUpdates, AccelerometerData} from "nativescript-accelerometer";
 import {TNSPlayer, AudioPlayerOptions} from 'nativescript-audio';
+
+import {zScore} from "../../shared/detection_algorithms/zscore";
+import {KalmanFilter} from "../../shared/detection_algorithms/kalmanjs";
+import {Label} from "ui/label";
+import {AbsoluteLayout} from "ui/layouts/absolute-layout";
+import {Animation} from "ui/animation";
+import {knownFolders} from "file-system"
 
 @Component({
     selector: "game",
     templateUrl: "pages/game/game.html"
 })
 export class GameComponent implements OnInit {
-    public scoreModel: number;
-    public score: number;
-
-    lastValue: number = 0;
-    buffer: Array<number> = [];
-    zscore = new zScore(5, 3.5, 0.1);
-    kalman = new KalmanFilter({R: 5, Q: 0.01});
+    private lastValue: number = 0;
+    private buffer: Array<number> = [];
+    private zscore = new zScore(5, 3.5, 0.1);
+    private kalman = new KalmanFilter({R: 5, Q: 0.01});
 
     private player: TNSPlayer;
+    private scoresLayout: AbsoluteLayout;
 
-    constructor(private zone: NgZone) {
+    // animation variables
+    private animationPositions: Array<number> = [20, 60, 100, 140, 180, 220];
+    private animationDirection: AnimationDirection = AnimationDirection.INCREASING;
+    private animationPosition: number = this.animationPositions.length / 2;
+
+    // score variables
+    private score: number = 0;
+    private scoreMultiplier: number = 1;
+
+    // beat variables
+    private beatArray: Array<number>;
+    private startTime: number;
+    private gestureMade: boolean = false;
+
+    constructor(private zone: NgZone, private page: Page) {
     }
 
     ngOnInit() {
-
-        this.score = 0;
-        this.scoreModel = 0;
-
+        this.scoresLayout = <AbsoluteLayout> this.page.getViewById("scoresLayout");
         this.player = new TNSPlayer();
-        this.playAudio('~/audio/faded.mp3', 'localFile');
 
-        startAccelerometerUpdates((data: AccelerometerData) => {
-            this.zone.run(() => {
-                var result = this.naiveFilter(data.z);
-                var output = this.detectSignalHigh(result);
-                if (output == 1) {
-                    this.scoreModel += 50;
-                }
-                //console.log(data.z);
+        // read beats file
+        knownFolders.currentApp().getFile("/audio/faded.csv").readText().then((text) => {
+            this.beatArray = text.split(",").map(function (value) {
+                return +value;
             });
+        }).then(() => {
+
+            // start playing song
+            this.playAudio("~/audio/faded.mp3", "localFile");
+
+            this.startTime = new Date().getTime();
+
+            // start accelerometer updates
+            startAccelerometerUpdates((data: AccelerometerData) => {
+                this.zone.run(() => {
+                    var result = this.naiveFilter(data.z);
+                    var signalHigh: boolean = this.detectSignalHigh(result);
+                    if (signalHigh) {
+                        this.gestureMade = true;
+                    }
+                    //console.log(data.z);
+                });
+            });
+
         });
 
-        // score on the view must be manually updated every 100ms, or it overuses the cpu and no update is processed
+        // view must be manually updated every 100ms or it overuses the cpu and no update is processed
         setInterval(() => {
-            this.score = this.scoreModel;
+
+            // if gesture was made check if actual beat exists
+            if (this.gestureMade) {
+
+                var actualBeatExists: boolean = false;
+                var timeSinceStartInSeconds: number = (new Date().getTime() - this.startTime) / 1000;
+                for (let beat of this.beatArray) {
+                    // beat at current time detected
+                    var diff = Math.abs(beat - timeSinceStartInSeconds);
+                    if (diff < 0.11) {
+                        actualBeatExists = true;
+                        break;
+                    }
+                }
+
+                var label = new Label();
+                label.cssClass = "big-text score-label";
+                label.text = "+" + (actualBeatExists ? this.scoreMultiplier : 0);
+
+                AbsoluteLayout.setLeft(label, this.animationPositions[this.animationPosition]);
+                AbsoluteLayout.setTop(label, 425);
+
+                this.scoresLayout.addChild(label);
+
+                var animation: Animation = label.createAnimation({
+                    translate: {x: 0, y: -425},
+                    opacity: 0.5,
+                    duration: 2000,
+                    scale: {x: 0.5, y: 0.5},
+                });
+
+                animation.play().then(() => {
+                    this.scoresLayout.removeChild(label);
+                });
+
+                if (actualBeatExists) {
+                    this.score += this.scoreMultiplier;
+                    this.scoreMultiplier++;
+                } else {
+                    this.scoreMultiplier = 1;
+                }
+
+                // adjust position
+                if (this.animationDirection == AnimationDirection.INCREASING && this.animationPosition < this.animationPositions.length - 1) {
+                    this.animationPosition++;
+                } else if (this.animationDirection == AnimationDirection.INCREASING && this.animationPosition == this.animationPositions.length - 1) {
+                    this.animationPosition--;
+                    this.animationDirection = AnimationDirection.DECREASING;
+                } else if (this.animationDirection == AnimationDirection.DECREASING && this.animationPosition > 0) {
+                    this.animationPosition--;
+                } else if (this.animationDirection == AnimationDirection.DECREASING && this.animationPosition == 0) {
+                    this.animationPosition++;
+                    this.animationDirection = AnimationDirection.INCREASING;
+                }
+
+                // reset beat detection states
+                this.gestureMade = false;
+            }
         }, 100);
 
     }
 
-    playAudio(filepath: string, fileType: string) {
+    private playAudio(filepath: string, fileType: string) {
         try {
             var playerOptions: AudioPlayerOptions = {
                 audioFile: filepath,
@@ -91,7 +178,7 @@ export class GameComponent implements OnInit {
      * Input must be new single value.
      * @param input
      */
-    kalmanFilter(input) {
+    private kalmanFilter(input) {
         return this.kalman.filter(input);
     }
 
@@ -99,28 +186,33 @@ export class GameComponent implements OnInit {
      * Input must be an array of all values until now.
      * @param input
      */
-    zscoreFilter(input) {
+    private zscoreFilter(input) {
         this.buffer.push(input);
         var result = this.zscore.filter(this.buffer);
         return result[result.length - 1];
     }
 
-    naiveFilter(input) {
-        if (input > 0.25) {
+    private naiveFilter(input) {
+        if (input > 0.185) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    detectSignalHigh(input) {
+    private detectSignalHigh(input) {
         var result;
         if (this.lastValue == 0 && input == 1) {
-            result = 1;
+            result = true;
         } else {
-            result = 0;
+            result = false;
         }
         this.lastValue = input;
         return result;
     }
+}
+
+enum AnimationDirection {
+    INCREASING,
+    DECREASING
 }
